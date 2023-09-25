3d61959f1de1df6d562e433d935ddad595470e4a
MirInjamamul <injamul120221@gmail.com>
2023-09-25 15:50:08 +0600

    revert

--- Controllers/RostersController.cs
+++ Controllers/RostersController.cs
@@ -104,7 +104,7 @@ namespace chat_server.Controllers
             return CreatedAtAction(nameof(Get), new { id = roster.Id }, roster);
         }
 
-        [HttpGet("last_online")]
+        [HttpPost]
         public ActionResult<List<Roster>> LastOnline([FromBody] UserIdModel userList)
         {
 
