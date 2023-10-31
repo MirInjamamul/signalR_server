5fd22c9a112cb2c5cc9195d854b3829227171127
MirInjamamul <injamul120221@gmail.com>
2023-10-31 20:58:15 +0600

    bug updated

--- Services/RosterService.cs
+++ Services/RosterService.cs
@@ -27,18 +27,18 @@ namespace chat_server.Services
             return _rosters.Find(roster => roster.UserId == id).FirstOrDefault();
         }
 
-        public List<Roster> GetOnlineRoster(string[] followerId)
+        public List<Roster> GetOnlineRoster(List<Follower> followers)
         {
-            var _onlineRoster = _rosters.Find(roster => followerId.Contains(roster.UserId) && roster.IsActive).ToList();
+            var _onlineRoster = _rosters.Find(roster => followers.Any(follower => follower.UserId == roster.UserId) && roster.IsActive).ToList();
 
             // Return first 12 or fewer if not avaialble
 
             return _onlineRoster.Take(12).ToList();
         }
 
-        public List<Roster> GetSuggestionRoster(string[] followerId)
+        public List<Roster> GetSuggestionRoster(List<Follower> followers)
         {
-            var _onlineRoster = _rosters.Find(roster => followerId.Contains(roster.UserId)).ToList();
+            var _onlineRoster = _rosters.Find(roster => followers.Any(follower => follower.UserId == roster.UserId)).ToList();
 
             Random random = new Random();
 
