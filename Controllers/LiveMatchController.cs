90a657d740099701f67aa272319a3c434f5012a2
MirInjamamul <injamul120221@gmail.com>
2023-11-07 01:50:17 +0600

    add api for send invitation

--- Controllers/LiveMatchController.cs
+++ Controllers/LiveMatchController.cs
@@ -3,6 +3,7 @@ using chat_server.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using MongoDB.Driver.Core.Connections;
 
 namespace chat_server.Controllers
 {
@@ -11,17 +12,26 @@ namespace chat_server.Controllers
     public class LiveMatchController : ControllerBase
     {
         private readonly IHubContext<ChatHub> _hubContext;
+        private readonly PresenceTracker _presenceTracker;
 
-        public LiveMatchController(IHubContext<ChatHub> hubContext)
+        public LiveMatchController(IHubContext<ChatHub> hubContext, PresenceTracker presenceTracker)
         {
             _hubContext = hubContext;
+            _presenceTracker = presenceTracker;
         }
 
         [HttpPost]
         [Route("PushInvitation")]
-        public IActionResult PushInvitation(String connectioId, LiveMatch invitation)
+        public IActionResult PushInvitation(String receiverId, LiveMatch invitation)
         {
-            _hubContext.Clients.Client(connectioId).SendAsync("ReceiveLiveInvitation", invitation);
+
+            List<UserDetail> userDetails = _presenceTracker.GetUserDetail(receiverId);
+
+            foreach (UserDetail userDetail in userDetails)
+            {
+                _hubContext.Clients.Client(userDetail.ConnectionId).SendAsync("ReceiveLiveInvitation", invitation);
+            }
+
 
             return Ok("Done");
         }
