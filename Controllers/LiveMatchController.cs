3d051b501b8f0a077f69bdf24b24b592e7758be3
MirInjamamul <injamul120221@gmail.com>
2023-11-28 12:27:39 +0600

    terminate streraming API connected

--- Controllers/LiveMatchController.cs
+++ Controllers/LiveMatchController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using MongoDB.Driver.Core.Connections;
+using static chat_server.Utils.Util;
 
 namespace chat_server.Controllers
 {
@@ -30,6 +31,36 @@ namespace chat_server.Controllers
             foreach (UserDetail userDetail in userDetails)
             {
                 _hubContext.Clients.Client(userDetail.ConnectionId).SendAsync("ReceiveLiveInvitation", invitation);
+            }
+
+
+            return Ok("Done");
+        }
+
+        [HttpPost]
+        [Route("StreamTermination")]
+        public IActionResult TerminateStream(String receiverId, int seatType)
+        {
+
+            List<UserDetail> userDetails = _presenceTracker.GetUserDetail(receiverId);
+
+            SeatType streamingSeatType;
+
+            switch (seatType) {
+                case 1:
+                    streamingSeatType = SeatType.single; break;
+                case 16:
+                    streamingSeatType = SeatType.pk; break;
+                default:
+                    Console.WriteLine($"SeatType doesn't match {seatType}");
+                    streamingSeatType = SeatType.none;
+                    break;
+
+            }
+
+            foreach (UserDetail userDetail in userDetails)
+            {
+                _hubContext.Clients.Client(userDetail.ConnectionId).SendAsync("ReceiveLiveInvitation", streamingSeatType);
             }
 
 
--- Utils/Util.cs
+++ Utils/Util.cs
@@ -4,5 +4,7 @@
     {
         // Story Type
         public enum StoryType { text, image, audio, video }
+
+        public enum SeatType { single, matching, three, four, six, nine, twelve, audio, auction, music, pk, none }
     }
 }
